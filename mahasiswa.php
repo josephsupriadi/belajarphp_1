52edfc7d35b20f551255d185dc6d772544e3c0bd
josephsupiradi <josephsupiradi86@gmail.com>
2022-10-13 09:35:54 +0700

    first

---
+++ mahasiswa.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <title>Insert ke database</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mt-4">
+
+                <div class="card">
+                    <div class="card-header">
+                        <h3>PHP_1
+                            <a href="index.php" class="btn btn-danger">Kembali</a>
+                        </h3>
+                    </div>
+                    <div class="card-body">
+
+                        <form action="" method="POST">
+                            <div class="mb-3">
+                                <label>Full Nama</label>
+                                <input type="text" name="fullname" class="form-control" />
+                            </div>
+                            <div class="mb-3">
+                                <label>Email</label>
+                                <input type="text" name="email" class="form-control" />
+                            </div>
+                            <div class="mb-3">
+                                <label>Phone</label>
+                                <input type="text" name="phone" class="form-control" />
+                            </div>
+                            <div class="mb-3">
+                                <label>Course</label>
+                                <input type="text" name="course" class="form-control" />
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
